14326e477bf7b5cdd348ed180317e7bfb00367ef
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-08-21 18:24:50 -0500

    WIP grouping data

--- src/components/Systems/JobClasses/index.tsx
+++ src/components/Systems/JobClasses/index.tsx
@@ -1,6 +1,6 @@
 import { group } from "console";
 import React from "react";
-import { IClass, IClassGroups } from "../../../global/types";
+import { IClass, IClassGroups, IClassRank, IClassRankGroups } from "../../../global/types";
 import { ClassIcon } from "../../UI";
 import css from './style.css'
 
@@ -17,9 +17,9 @@ export const parseClassData = (data: Array<IClass>) => {
 
         // Check Skill Point Rank
         if (typeof rank === 'number') {
-            if (rank >= 10) rank = 'レア - Rare';
-            else if (rank >= 5) rank = 'ハイ - High';
-            else rank = 'ベース - Base';
+            if (rank >= 10) fd["ランク - Rank"] = 'レア - Rare';
+            else if (rank >= 5) fd["ランク - Rank"] = 'ハイ - High';
+            else fd["ランク - Rank"] = 'ベース - Base';
         }
         return fd;
     }).filter(d => d !== null) as Array<IClass> | null;
@@ -29,19 +29,50 @@ export const parseClassData = (data: Array<IClass>) => {
     return null;
 }
 
+// Assumptions
+// Classes are base jobs
+// Jobs are Advanced classes
+// Rare are even more advanced classes
+// 15 > 10 > 5
+
+const groupRanks = (data: IClassGroups) => {
+
+}
+
 // Group data by classification
-export const groupClassses = (data: Array<IClass>) => {
+const groupClassses = (data: Array<IClass>) => {
     // Get all possible classifications
     let classifications = [] as Array<string>;
     classifications = data.map(d => d["分類 - classification"]);
     classifications = Array.from(new Set(classifications));
 
+    let ranks = [] as Array<string>;
+    ranks = data.map(d => d["ランク - Rank"] as string);
+    ranks = Array.from(new Set(ranks)).filter(r => !!r);
+    console.log('ranks:', ranks);
+
     // Start Making a group for each classification
     const groups = {} as IClassGroups;
+    const rankedgroups = {} as IClassRankGroups;
     // Create groups
-    classifications.forEach(cl => { groups[cl] = data.filter(d => d["分類 - classification"] === cl) as Array<IClass> });
+    classifications.forEach(cl => {
+        groups[cl] = data.filter(d => d["分類 - classification"] === cl) as Array<IClass>;
+        rankedgroups[cl] = [] as Array<IClassRank>;
 
-    return groups;
+
+        ranks.forEach((rank) => {
+            const group = groups[cl];
+
+            // console.log(`Adding ${rank}:`, group)
+
+            rankedgroups[cl][rank] = group.filter(c => c["ランク - Rank"] === rank);
+        });
+
+    });
+
+    console.log({ rankedgroups })
+
+    return rankedgroups;
 }
 
 export const JobClasses = ({ data }: { data: IClassGroups }) => {
@@ -49,14 +80,36 @@ export const JobClasses = ({ data }: { data: IClassGroups }) => {
     const getClassIcons = () => {
         const cfs = Object.keys(data);
 
-        return cfs.map(c => {
-            const jobClass = data[c];
+        console.log('data:', data)
 
+        // Render classfications
+        return cfs.map(c => {
+            const jobGroups = data[c];
+
+            // Get ranks
+            let ranks = [] as Array<string>;
+            ranks = jobGroups.map(group => group["ランク - Rank"] as string);
+            ranks = Array.from(new Set(ranks));
+
+            // Render Rank Groups
             return <div className='classification' key={c}>
-                {jobClass?.map((d) => {
-                    const cls = d["クラス - Class"];
-                    return <ClassIcon key={cls}>{cls}</ClassIcon>;
-                })}
+                <h3>{c}</h3>
+                {
+                    ranks.map(rank => {
+                        console.log({ rank });
+
+                        // Job Classes
+                        return <div>
+                            <h3>{rank}</h3>
+                            {
+                                jobGroups[rank]?.map((group) => {
+                                    const cls = group["クラス - Class"];
+                                    return <ClassIcon key={cls}>{cls}</ClassIcon>;
+                                })
+                            }
+                        </div>
+                    })
+                }
             </div>
         })
 
@@ -64,6 +117,7 @@ export const JobClasses = ({ data }: { data: IClassGroups }) => {
     const icons = getClassIcons();
 
     return <div className={css['classes']}>
+        <h2></h2>
         {icons}
     </div>
 }
--- src/global/types.ts
+++ src/global/types.ts
@@ -7,6 +7,22 @@ export interface IClass {
     'Passive Skills': string;
     'Details': string;
 }
+
 export interface IClassGroups {
     [index: string | number]: Array<IClass>
+}
+
+export enum CLASS_RANKS {
+    '不明 - undefined',
+    'ベース - Base',
+    'ハイ - High',
+    'レア - Rare'
+}
+
+export interface IClassRank {
+    [index: string | number | CLASS_RANKS]: Array<IClass>;
+}
+
+export interface IClassRankGroups {
+    [index: string | number]: Array<IClassRank>;
 }
