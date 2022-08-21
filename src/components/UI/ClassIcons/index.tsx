789bf839b3c19c5cd739c5e86fc32836ed4b59e2
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-08-21 16:32:59 -0500

    Cleaned up class icon style

--- src/components/UI/ClassIcons/index.tsx
+++ src/components/UI/ClassIcons/index.tsx
@@ -2,5 +2,9 @@ import React from "react"
 import css from './style.css';
 
 export const ClassIcon = ({ label, children }: { label?: string, children?: string }) => {
-    return <div className={css["interface-icon"]}>{label || children}</div>
+    return <div className={css["interface-icon"]}>
+        <div >
+            <label>{label || children}</label>
+        </div>
+    </div>
 }
